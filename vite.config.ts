a8a23f847d62ec649dfce5e94041ea7b83e91b08
Ada Chan (ITS-DT) <adachan@yflife.com>
2026-09-02 15:42:37 +0800

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/look-outside/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
